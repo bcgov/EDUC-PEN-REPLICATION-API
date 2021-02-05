6ee84ea2a0f598d910ac2172490db7d1127338af
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-02-05 10:20:19 -0800

    code changes to handle student update...

--- api/src/main/java/ca/bc/gov/educ/api/pen/replication/struct/Student.java
+++ api/src/main/java/ca/bc/gov/educ/api/pen/replication/struct/Student.java
@@ -1,7 +1,6 @@
 package ca.bc.gov.educ.api.pen.replication.struct;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -14,7 +13,6 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 @SuppressWarnings("java:S1948")
 public class Student extends BaseStudent implements Serializable {
