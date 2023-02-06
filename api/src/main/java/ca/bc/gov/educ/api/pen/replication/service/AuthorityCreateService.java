08b4e056e8f0381f4cf7c7093db35b729b98f2f7
arcshiftsolutions <arcshiftsolutions@gmail.com>
2023-02-06 14:23:29 -0800

    Added create & update of authorities.

---
+++ api/src/main/java/ca/bc/gov/educ/api/pen/replication/service/AuthorityCreateService.java
@@ -0,0 +1,52 @@
+package ca.bc.gov.educ.api.pen.replication.service;
+
+import ca.bc.gov.educ.api.pen.replication.mappers.AuthorityMapperHelper;
+import ca.bc.gov.educ.api.pen.replication.model.Event;
+import ca.bc.gov.educ.api.pen.replication.repository.AuthorityMasterRepository;
+import ca.bc.gov.educ.api.pen.replication.repository.EventRepository;
+import ca.bc.gov.educ.api.pen.replication.struct.IndependentAuthority;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import static ca.bc.gov.educ.api.pen.replication.constants.EventType.CREATE_AUTHORITY;
+
+@Service
+@Slf4j
+public class AuthorityCreateService extends BaseService<IndependentAuthority> {
+
+  private final AuthorityMapperHelper authorityMapperHelper;
+  private final AuthorityMasterRepository authorityMasterRepository;
+
+  public AuthorityCreateService(final EntityManagerFactory emf, final EventRepository eventRepository, AuthorityMapperHelper authorityMapperHelper, AuthorityMasterRepository authorityMasterRepository) {
+    super(eventRepository, emf);
+    this.authorityMapperHelper = authorityMapperHelper;
+    this.authorityMasterRepository = authorityMasterRepository;
+  }
+
+  @Override
+  public void processEvent(final IndependentAuthority authority, final Event event) {
+    log.info("Received and processing event: " + event.getEventId());
+
+    val existingSchoolMasterRecord = this.authorityMasterRepository.findById(authority.getAuthorityNumber());
+    if (!existingSchoolMasterRecord.isPresent()) {
+      val newAuthorityMaster = authorityMapperHelper.toAuthorityMaster(authority, true);
+      log.info("Processing choreography update event with ID {} :: payload is: {}", event.getEventId(), newAuthorityMaster);
+      authorityMasterRepository.save(newAuthorityMaster);
+    }
+    this.updateEvent(event);
+  }
+
+  @Override
+  public String getEventType() {
+    return CREATE_AUTHORITY.toString();
+  }
+
+  @Override
+  protected void buildAndExecutePreparedStatements(final EntityManager em, final IndependentAuthority authority) {
+    // Not required this child class use repository pattern of spring.
+  }
+}
